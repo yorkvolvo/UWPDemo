3b7db9ab4c007f041b2572634c5f9f05b1308e4a
yang shen <shenyanglizi@outlook.com>
2017-03-24 13:39:29 +0800

    xBindListExample

---
+++ xBindListExample/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xBindListExample.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AvatarPath { get; set; }
+
+    }
+}
